fc0e62aaefcd251082af12a8d6ec2838f1fe5cf2
tuhhy <tuhhy@protonmail.com>
2022-12-01 07:01:44 +0100

    aoc 2022

---
+++ Day1/TaskA.py
@@ -0,0 +1,23 @@
+from operator import indexOf
+
+lst = []
+with open('input.txt') as f:
+    lst = f.readlines()
+print(lst)
+
+maxint = []
+group = 0
+for i in lst:
+    if i.strip() == '':
+        maxint.append(group)
+        group = 0
+    else:
+        group += int(i.strip())
+
+print(max(maxint))
+
+wirklichmax = []
+for i in range(3):
+    wirklichmax.append(maxint.pop(indexOf(maxint, max(maxint))))
+print(wirklichmax)
+print(sum(wirklichmax))
